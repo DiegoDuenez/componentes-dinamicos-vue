2b796888d91713ce4a58666acb6def75afc830a8
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-03-02 15:31:38 -0600

    init :)

---
+++ src/components/navbar/navbar.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <nav class="navbar">
+
+    </nav>
+    
+</template>
+
+
+<script>
+export default {
+  name: 'NavigationVue'
+}
+</script>
+
+
+<style scoped>
+
+    .navbar{
+        display: flex;
+        width: 100%;
+        height: 5rem;
+        background-color: red;
+        justify-content: space-between;
+    }
+
+    .navbar__links{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: rgb(51, 255, 0);
+
+    }
+    
+    .navbar__logo{
+        display: flex;
+        width: 30%;
+        height: 100%;
+        background-color: rgb(38, 0, 255);
+    }
+
+</style>
